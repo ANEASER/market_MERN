885d4704f98543e34a383ddff8ee2d415be9bea1
ANEASER <anudaattanayake@gmail.com>
2023-06-14 13:02:28 +0530

    OTP sending added

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 
 const  userControllers = require('../controller/usercontrollers');
-
+const Auth = require('../middleware/auth');
 
 router.post("/register", userControllers.registerUser);
 router.post("/login", userControllers.loginUser);
 router.get("/profile",userControllers.UserProfile);
 router.get("/logout", userControllers.Logout);
+
+router.route("/generateOTP").get(userControllers.verifyUser,Auth.localVariable,userControllers.generateOTP);
+
+router.get('/verifyOTP',userControllers.verifyOTP);
+router.get('/createresetsession',userControllers.createResetSession);
+
+router.route('/resetpassword').put(userControllers.verifyUser, userControllers.resetPassword);
 
 module.exports = router;
